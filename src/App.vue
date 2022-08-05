9a00c0c7851e3728e21c93123377cc1832ba09c2
KaiKaixxxl <863217717@qq.com>
2022-08-05 11:06:32 +0800

    日常更新

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #3a526a;
+  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
