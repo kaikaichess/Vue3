f2b4b06544ba1511afdda71d77d33cb4b0a99423
KaiKaixxxl <863217717@qq.com>
2022-08-05 20:08:49 +0800

    日常更新

---
+++ 02_src初识setup/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>个人信息</h1>
+  <h2>姓名： {{name}}</h2>
+  <h2>年龄： {{age}}</h2>
+  <button @click="sayHello">你好</button>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  // 测试setup
+  setup() {
+    let name = '张三'
+    let age = 18
+    function sayHello() {
+      console.log(`你好，我是${name},我今年${age}岁了`);
+    }
+
+    return {
+      name,
+      age,
+      sayHello
+    }
+
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ 03_srcref函数/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>个人信息</h1>
+  <h2>姓名： {{name}}</h2>
+  <h2>年龄： {{age}}</h2>
+  <h3>工作种类：{{job.type}}</h3>
+  <h3>工作薪水：{{job.salary}}</h3>
+  <button @click="changeInfo">修改人的信息</button>
+</template>
+
+<script>
+  import {ref} from 'vue'
+  export default {
+    name: 'App',
+    // 测试setup
+    setup() {
+      let name = ref('张三')
+      let age = ref(18)
+      let job = ref({
+        type: '前端',
+        salary: '30K'
+      })
+      function changeInfo() {
+        name.value = '李四'
+        age.value = 48
+        job.value.type = "UI设计师"
+        job.value.salary = '40K'
+      }
+
+      return {
+        name,
+        age,
+        job,
+        changeInfo
+      }
+
+    },
+  }
+</script>
+
+<style>
+
+</style>
